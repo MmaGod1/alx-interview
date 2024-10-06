6ba513c6b91a11c8cb40259af5dbe963bc208e54
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-06 13:19:14 +0100

    Pycodestyle check

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Module for generating Pascal's Triangle.
 """
 
+
 def pascal_triangle(n):
     """
     Generate Pascal's Triangle up to n rows.
@@ -16,13 +17,13 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = []
-    
+
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-    
+
     return triangle
