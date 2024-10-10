ffd8150ce67896ba0c82f2f620cb2782a52a5761
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-10-10 07:04:53 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines the canUnlockAll function to determine if all boxes 
+in a list of locked boxes can be opened using keys found in the boxes.
+"""
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+    
+    Args:
+        boxes (list): A list of lists where each sublist contains keys 
+                      to other boxes.
+    
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
     unlocked = [0]
     keys = set(boxes[0])
 
