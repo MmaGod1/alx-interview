3bf7f7fe09fd39bc1eb1482d3f5633afd6f37590
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-04 17:11:52 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
+ """Reads stdin line by line and computes metrics"""
 import sys
-"""Reads stdin line by line and computes metrics"""
 
 
 count_line = 0
 add_file_size = 0
-status_count = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
+status_count = {'200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
         line = line.strip()
         line_str = line.split()
-        
+
         if len(line_str) > 7:
-            status = line_str[-2]
-            file_size = int(line_str[-1])
+            status = line_str[6]
+            file_size = int(line_str[7])
+            add_file_size += file_size
 
             count_line += 1
-            add_file_size += file_size
 
             if status in status_count:
                 status_count[status] += 1
@@ -29,10 +29,10 @@ try:
                     if status_count[code] > 0:
                         print(f"{code}: {status_count[code]}")
 
-except Exception:
+except (KeyboardInterrupt, ValueError):
     pass
-finally:
-    print(f"File size: {add_file_size}")
-    for code in sorted(status_count.keys()):
-        if status_count[code] > 0:
-            print(f"{code}: {status_count[code]}")
+
+print(f"File size: {add_file_size}")
+for code in sorted(status_count.keys()):
+    if status_count[code] > 0:
+        print(f"{code}: {status_count[code]}")
