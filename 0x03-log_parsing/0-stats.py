9affea61317d989f08f635605d5adc96f3df9c0e
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-04 17:19:14 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,17 +3,21 @@
 import sys
 
 
+#!/usr/bin/python3
+ """Reads stdin line by line and computes metrics"""
+import sys
+
+
 count_line = 0
 add_file_size = 0
 status_count = {'200': 0, '301': 0, '400': 0, '401': 0,
                 '403': 0, '404': 0, '405': 0, '500': 0}
 
-
 try:
     for line in sys.stdin:
         line = line.strip()
         line_str = line.split()
-
+        
         if len(line_str) > 7:
             status = line_str[6]
             file_size = int(line_str[7])
@@ -30,7 +34,7 @@ try:
                     if status_count[code] > 0:
                         print(f"{code}: {status_count[code]}")
 
-except (KeyboardInterrupt, ValueError):
+except KeyboardInterrupt:
     pass
 finally:
     print(f"File size: {add_file_size}")
