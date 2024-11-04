88c7bc570a27989aaf19668f4ac03ce72610bb83
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-04 17:16:17 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,6 +8,7 @@ add_file_size = 0
 status_count = {'200': 0, '301': 0, '400': 0, '401': 0,
                 '403': 0, '404': 0, '405': 0, '500': 0}
 
+
 try:
     for line in sys.stdin:
         line = line.strip()
@@ -16,9 +17,9 @@ try:
         if len(line_str) > 7:
             status = line_str[6]
             file_size = int(line_str[7])
-            add_file_size += file_size
 
             count_line += 1
+            add_file_size += file_size
 
             if status in status_count:
                 status_count[status] += 1
@@ -31,8 +32,8 @@ try:
 
 except (KeyboardInterrupt, ValueError):
     pass
-
-print(f"File size: {add_file_size}")
-for code in sorted(status_count.keys()):
-    if status_count[code] > 0:
-        print(f"{code}: {status_count[code]}")
+finally:
+    print(f"File size: {add_file_size}")
+    for code in sorted(status_count.keys()):
+        if status_count[code] > 0:
+            print(f"{code}: {status_count[code]}")
