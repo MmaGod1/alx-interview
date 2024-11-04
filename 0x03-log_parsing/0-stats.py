e791cee354e5a3025bdf5582c8bb11f9a0727e45
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-04 17:22:07 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,16 +4,16 @@ import sys
 
 count_line = 0
 add_file_size = 0
-status_count = {'200': 0, '301': 0, '400': 0, '401': 0,
-                '403': 0, '404': 0, '405': 0, '500': 0}
+status_count = {200: 0, 301: 0, 400: 0, 401: 0,
+                403: 0, 404: 0, 405: 0, 500: 0}
 
 try:
     for line in sys.stdin:
         line = line.strip()
         line_str = line.split()
-        
+
         if len(line_str) > 7:
-            status = line_str[6]
+            status = int(line_str[6])
             file_size = int(line_str[7])
 
             count_line += 1
