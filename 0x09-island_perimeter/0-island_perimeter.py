ed3484419a325c610b3b45ef1d96c0ce6bc0a054
MmaGod1 <gmmaduchukwu@gmail.com>
2024-12-06 06:58:16 -0800

    Island Perimiter calculation

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Island Perimeter
+"""
+
+
+def island_perimeter_original(grid):
+    """ Calculates an island primeter. """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if j == 0 or grid[i][j - 1] != 1:
+                    perimeter += 1
+                if i == 0 or grid[i - 1][j] != 1:
+                    perimeter += 1
+                if j + 1 == len(grid[0]) or grid[i][j + 1] != 1:
+                    perimeter += 1
+                if i + 1 == len(grid) or grid[i + 1][j] != 1:
+                    perimeter += 1
+    return perimeter
